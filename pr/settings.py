23ed4dea3ef676b210b2d75ab725230d09aebdd1
anu-antiha <gadhamanitha@gmail.com>
2022-02-04 15:36:03 +0530

    sixth correct commit

--- pr/settings.py
+++ pr/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&)n#r+1(-he*q_ac5!2n(w53i*ty4q!yj=(no4j6%34mb!yfae'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://obscure-ravine-40920.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
