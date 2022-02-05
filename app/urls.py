5a6fabca8f21e73c5f847d6df287d0bb55509ddf
anu-antiha <gadhamanitha@gmail.com>
2022-02-05 12:55:27 +0530

    9th commit

--- app/urls.py
+++ app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns= [
-	path('home/',views.home,name="home") ,
+	path('',views.home,name="home") ,
 	path('create/', views.create, name='create'),
 	path('update/<str:id>/', views.update, name='update'),
     path('read/<str:id>/', views.read, name='read'), #read/1/
--- pr/urls.py
+++ pr/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
-    path('', views.loginn, name='login'),
+    path('login/', views.loginn, name='login'),
     path('logout/', views.logoutt, name='logout'),
     path('signup/', views.signup, name='signup'),
 
